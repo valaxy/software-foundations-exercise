b83407e1b2c34bbc9aa70541bda4dcae2551d4b1
valaxy <valaxy@qq.com>
2017-02-06 20:17:32 +0800

    update basics & readme

--- Basics.v
+++ Basics.v
@@ -1,13 +1,15 @@
 Fixpoint beq_nat (n m : nat) : bool :=
   match n with
-  | O => match m with
-         | O => true
-         | S m' => false
-         end
-  | S n' => match m with
-            | O => false
-            | S m' => beq_nat n' m'
-            end
+  | O => 
+    match m with
+    | O => true
+    | S m' => false
+    end
+  | S n' => 
+    match m with
+    | O => false
+    | S m' => beq_nat n' m'
+    end
   end.
 
 
--- readme.md
+++ readme.md
@@ -1,1 +1,1 @@
-Exercise of [Software Foundations](http://www.cis.upenn.edu/~bcpierce/sf/current/index.html), a book about coq.
+Exercise of [Software Foundations](http://www.cis.upenn.edu/~bcpierce/sf/current/index.html), a book about coq, since version 4.0 (May, 2016).
