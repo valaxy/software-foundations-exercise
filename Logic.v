9656f62bf82ec09762339817c8b1e9f63131f426
valaxy <valaxy@qq.com>
2017-02-18 20:45:04 +0800

    more exercises

--- Logic.v
+++ Logic.v
@@ -1,3 +1,5 @@
+Require Import Coq.Setoids.Setoid.
+
 (** **** Exercise: 2 stars (and_exercise)  *)
 Example and_exercise: forall n m: nat, n + m = 0 -> n = 0 /\ m = 0.
 Proof.
@@ -101,8 +103,89 @@ Qed.
 
 
 
+(** **** Exercise: 1 star, optional (iff_properties)  *)
+Theorem iff_refl: forall P: Prop, P <-> P.
+Proof.
+  intros P.
+  split.
+  - intros.
+    apply H.
+  - intros.
+    apply H.
+Qed.      
 
 
+
+Theorem iff_trans: forall P Q R: Prop,
+  (P <-> Q) -> (Q <-> R) -> (P <-> R).
+Proof.
+  intros P Q R [H1 H2] [H3 H4].
+  split.
+  - intros.
+    apply H3.
+    apply H1.
+    apply H.
+  - intros.
+    apply H2.
+    apply H4.
+    apply H.
+Qed.
+
+
+(** **** Exercise: 3 stars (or_distributes_over_and)  *)
+Theorem or_distributes_over_and: forall P Q R: Prop,
+  P \/ (Q /\ R) <-> (P \/ Q) /\ (P \/ R).
+Proof.
+  intros P Q R.
+  split.
+  - intros [H1 | H2].
+    + split.
+      * left. assumption.
+      * left. assumption.
+    + assert (Q). { apply proj1 in H2. assumption. }
+      assert (R). { apply proj2 in H2. assumption. }
+      split.
+      * right. assumption.
+      * right. assumption.
+  - intros [[H1|H2] [H3|H4]].
+    + left. apply H1.
+    + left. apply H1.
+    + left. apply H3.
+    + right. split. assumption. assumption.
+Qed.
+
+
+
+(** **** Exercise: 1 star (dist_not_exists)  *)
+Theorem dist_not_exists: forall (X: Type)(P: X -> Prop),
+  (forall x, P x) -> ~ (exists x, ~ P x).
+Proof.
+  unfold not.
+  intros X P S [x].
+  apply H.
+  apply S.
+Qed.
+  
+
+(** **** Exercise: 2 stars (dist_exists_or)  *)
+Theorem dist_exists_or: forall (X: Type) (P Q: X -> Prop),
+  (exists x, P x \/ Q x) <-> (exists x, P x) \/ (exists x, Q x).
+Proof.
+  intros X P Q.
+  split.
+  - intros [x0 [H1|H2]].
+    + left. exists x0. assumption.
+    + right. exists x0. assumption.
+  - intros [[x0 H] | [x0 H]].
+    + exists x0. left. assumption.
+    + exists x0. right. assumption.
+Qed.
+
+
+(* exercises *)
+
+
+       
 
 
 
---
+++ Rel.v
@@ -0,0 +1,1 @@
+Check le: relation nat.
